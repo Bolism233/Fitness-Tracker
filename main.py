4d17771805fbe7c0e0942e3640db21edd7055a99
Salmaan Ejaz <sejaz1@binghamton.edu>
2021-10-27 15:21:47 -0400

    first commit

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import pygame
 #import your controller
 
 def main():
-    #Create an instance on your controller object
-    #Call your mainloop
+	pygame.init()
+	team = {"lead": "Salmaan", "backend": "Shijun", "frontend": "Lucas"}
+	print("Software Lead is:", team["lead"])
+	print("Backend is:", team["backend"])
+	print("Frontend is:" , team["frontend"])    
+
+					#Create an instance on your controller object
+    					#Call your mainloop
     
     ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 2 LINES OF CODE ######
 main()
