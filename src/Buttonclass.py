6f00ec9296496f3351ba40e34a19196e2189d076
Jay You <sj.jayyou@gmail.com>
2021-11-10 10:15:01 -0500

    trying to build button class

--- src/Buttonclass.py
+++ src/Buttonclass.py
@@ -1,10 +1,33 @@
 import pygame
 
-class Button(pygame.sprite.Sprite):
-    def __init__(self):
-        super.__init__(self)
-        self.image = pygame.image.load("assets/calculateButton.jpg")
+screen_width = 800
+screen_height = 500
+btn_image = pygame.image.load("calculateButton2.jpg")
+btn_image= pygame.transform.rotate(pygame.transform.scale(btn_image, (80,50)), 0)
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+class Button:
+    def __init__(self, x, y, image):
+        self.image = image
         self.rect = self.image.get_rect()
-        self.rect.x = 450
-        self.rect.y = 500
+        self.rect.topleft = (x,y)
+
+    def draw(self):
+        #draw button on screen
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+start_button = Button(100, 200, btn_image)
+exit_button = Button(450, 200, btn_image)
+
+run = True
+while run:
+    screen.fill((202, 229, 241))
+    start_button.draw()
+    exit_button.draw()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+pygame.quit()
 
