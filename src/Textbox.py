d7c11ddccb3fb12297df632f07ee85a51a7377c4
Jay You <sj.jayyou@gmail.com>
2021-11-11 21:08:52 -0500

    not important

--- src/Textbox.py
+++ src/Textbox.py
@@ -1,9 +1,19 @@
 import pygame
 
 
-class Textbox:
-    def __init__(self, user_input, surface, coord):
-        base_font = pygame.font.Font(None, 32)
-        user_text = user_input
-        text_surface = base_font.render(user_text, True, (255,255,255)) #.render text, anti-alias, color
-        surface.blit(text_surface, (0,0))
+class Button(pygame.sprite.Sprite):
+
+    # Constructor. Pass in the color of the block,
+    # and its x and y position
+    def __init__(self, color, width, height):
+       # Call the parent class (Sprite) constructor
+       pygame.sprite.Sprite.__init__(self)
+
+       # Create an image of the block, and fill it with a color.
+       # This could also be an image loaded from the disk.
+       self.image = pygame.Surface([width, height])
+       self.image.fill(color)
+
+       # Fetch the rectangle object that has the dimensions of the image
+       # Update the position of this object by setting the values of rect.x and rect.y
+       self.rect = self.image.get_rect()
